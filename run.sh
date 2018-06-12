27ff8a7e13995132f4f10bca574f1067fe7da9ae
Worawut Boontan <worawut@theiconweb.com>
2018-06-12 10:44:11 +0700

    Restart Alway s

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 docker rm -f zabbix
-docker run --name zabbix -v /etc/localtime:/etc/localtime -v $(pwd)/config:/etc/zabbix -v $(pwd)/php5:/etc/php5:ro -v zabbix:/var/lib/mysql -p 8000:80 -p 10051:10051 -d zabbix/zabbix-appliance
+docker run --name zabbix --restart=always -v /etc/localtime:/etc/localtime -v $(pwd)/config:/etc/zabbix -v $(pwd)/php5:/etc/php5:ro -v zabbix:/var/lib/mysql -p 8000:80 -p 10051:10051 -d zabbix/zabbix-appliance
 docker logs -f zabbix
